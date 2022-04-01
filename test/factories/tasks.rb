134d8e9ef0f6a35fd5bb1c0f6e0629018f414735
art v <artvpublic@gmail.com>
2022-04-01 12:01:57 +0300

    add user-task relations

--- test/factories/tasks.rb
+++ test/factories/tasks.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :task do
-    name { "MyString" }
-    description { "MyText" }
-    author_id { 1 }
-    assignee_id { 1 }
-    state { "MyString" }
+    name
+    description
+    author factory: manager
+    assignee_id factory: developer
+    state
     expired_at { "2022-03-31" }
   end
 end
